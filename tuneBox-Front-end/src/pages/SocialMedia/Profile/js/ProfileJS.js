12e4911094d882485c1d2501739ccf0184180da0
halayra <quoctrung25112019@gmail.com>
2024-10-03 10:39:42 +0700

    sua cong, tu them tay may thang ca si ok? talen so thich, tu them tay di may dua anh khong them cho may dua dau? oke?

--- tuneBox-Front-end/src/pages/SocialMedia/Profile/js/ProfileJS.js
+++ tuneBox-Front-end/src/pages/SocialMedia/Profile/js/ProfileJS.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchDataUser = async (userId) => {
     try {
-        const response = await axios.get(`http://localhost:8081/user/get/${userId}`, { withCredentials: true });
+        const response = await axios.get(`http://localhost:8080/user/get/${userId}`, { withCredentials: true });
         const data = response.data;
         console.log('User data fetched from API:', data);
         return data; 
