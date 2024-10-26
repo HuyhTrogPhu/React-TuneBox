017e6953d5d2a5a58cc16f32d5e2e58d6d48a05b
Your Name <you@example.com>
2024-10-26 21:01:56 +0700

    update navbar and feed

--- tuneBox-Front-end/src/pages/SocialMedia/Profile/ProfileUser.jsx
+++ tuneBox-Front-end/src/pages/SocialMedia/Profile/ProfileUser.jsx
@@ -13,7 +13,7 @@ import "./css/comment.css";
 import "./css/modal-create-post.css";
 import { images } from "../../../assets/images/images";
 import { FollowContext } from './FollowContext';
-import { getUserInfo, getFollowCountByUserId } from "../../../service/UserService";
+
 
 const ProfileUser = () => {
   const userIdCookie = Cookies.get("userId");
