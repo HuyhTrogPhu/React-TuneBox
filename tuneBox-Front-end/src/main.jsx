7ebea9be001a556606db49aa16974d25447719b5
halayra <quoctrung25112019@gmail.com>
2024-11-12 20:19:42 +0700

    login/signup with google

--- tuneBox-Front-end/src/main.jsx
+++ tuneBox-Front-end/src/main.jsx
@@ -10,7 +10,7 @@ import 'jquery/dist/jquery.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
-const clientId = "568812302293-nisnhgl6imji9qtsn6353dlvvf420vur.apps.googleusercontent.com"; 
+const clientId = "28392767205-kjh3koov94lcf8d2dhh83o15siro23m7.apps.googleusercontent.com"; 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
      <GoogleOAuthProvider clientId={clientId}>
