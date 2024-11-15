b3273a8b1dc558d2edd93db17638cb4eb2f186ed
phamhoaiphong-dev <phongphdev@gmail.com>
2024-11-15 15:47:46 +0700

    cai gi day

--- tuneBox-Front-end/src/pages/SocialMedia/Profile/Profile_nav/WaveFormFeed.jsx
+++ tuneBox-Front-end/src/pages/SocialMedia/Profile/Profile_nav/WaveFormFeed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import WaveSurfer from "wavesurfer.js";
 import "./css/waveFormFeed.css";
 import { images } from "../../../../assets/images/images";
+import { Link } from 'react-router-dom';
 
 const WaveFormFeed = ({ audioUrl, track }) => {
     const waveformRef = useRef(null);
