2c3dc122b4e4dfd486d2252cc3667c7f77532735
Huỳnh Trọng Phú <117765537+HuyhTrogPhu@users.noreply.github.com>
2024-11-29 11:24:01 +0700

    Delete tuneBox-Front-end directory

--- tuneBox-Front-end/src/components/ui/input.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/ui/input.jsx
-import React from 'react';
-
-const Input = ({ placeholder }) => {
-  return <input type="text" placeholder={placeholder} className="border p-2 rounded" />;
-};
-
-export { Input };
--- tuneBox-Front-end/src/service/CheckoutService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const REST_API_BASE_URL = 'http://localhost:8080/customer/checkout';
-
-export const getUserById = (userId) => axios.get(`${REST_API_BASE_URL}/getUserById/${userId}`);
-
-
-// get order by user id
-export const getOrdersByUserId = async (userId) => axios.get(`${REST_API_BASE_URL}/orders/${userId}`);
