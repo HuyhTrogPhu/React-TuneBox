b03dba7f720b42bbf25735324b9f418805dadacf
Pham Xuan Truong <122740745+sonnpxt04@users.noreply.github.com>
2024-12-07 16:51:33 +0700

    edit app

--- tuneBox-Front-end/src/App.jsx
+++ tuneBox-Front-end/src/App.jsx
@@ -107,8 +107,8 @@ function AppContent(){
           <Routes>
             {/* Các route có Header */}
             <Route element={<LayoutWithHeader />}>
-              {/* <Route path="/*" element={<XacThucUserLogin><HomeFeed /></XacThucUserLogin>} /> */}
-              <Route path="/*" element={<HomeFeed />} />
+              <Route path="/*" element={<XacThucUserLogin><HomeFeed /></XacThucUserLogin>} />
+      
               <Route path="/HomeEcommerce" element={<Home />} />
               {/* Route cho Main Content */}
               <Route path="/Shop" element={<Shop />} />
